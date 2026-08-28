474ad7497e68888afcc5fe515118f5ef3021ba5a
Jeanfranco Flores Valencia <jeanfranco.flores@tecsup.edu.pe>
2026-08-28 02:04:26 -0500

    Agrega reporte de detalle con columnas alineadas

--- Lab02CarritoKotlin/app/src/main/java/com/floresvalencia/lab02carritokotlin/Carrito.kt
+++ Lab02CarritoKotlin/app/src/main/java/com/floresvalencia/lab02carritokotlin/Carrito.kt
@@ -37,5 +37,27 @@ fun main() {
     for (producto in carrito) {
         println("Producto agregado: ${producto.nombre}")
     }
+    println()
+    mostrarDetalle(carrito)
 
+    println("Cantidad de productos : ${carrito.size}")
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+    println(String.format("%-22s: S/ %7.2f", "Subtotal", subtotal))
+    println(String.format("%-22s: S/ %7.2f", "IGV (18%)", igv))
+    println(String.format("%-22s: S/ %7.2f", "TOTAL A PAGAR", total))
+    println("-----------------------------------------")
+}
+fun mostrarDetalle(productos: List<Producto>) {
+    println("--------- DETALLE DEL CARRITO ---------")
+    var i = 1
+    for (p in productos) {
+        val importe = p.precio * p.cantidad
+        println(String.format("%d. %-20s x%d  S/ %8.2f", i, p.nombre, p.cantidad, importe))
+        i++
+    }
+    println("-----------------------------------------")
 }
