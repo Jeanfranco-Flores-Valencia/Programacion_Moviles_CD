90941c144c1ea0836b6718c8e61a5ed12c31fc86
Jeanfranco Flores Valencia <jeanfranco.flores@tecsup.edu.pe>
2026-08-28 03:33:53 -0500

    implementar subclases ProductoFisico y ProductoDigital con polimorfismo

--- Semana02/app/src/main/java/com/floresvalencia/lab02carritokotlin/Producto.kt
+++ Semana02/app/src/main/java/com/floresvalencia/lab02carritokotlin/Producto.kt
@@ -21,4 +21,37 @@ abstract class Producto(
     open fun obtenerDetalle(): String {
         return String.format("%-25s x%-2d  S/ %8.2f", nombre, cantidad, calcularPrecioFinal())
     }
+    class ProductoFisico(
+        nombre: String,
+        precio: Double,
+        cantidad: Int,
+        val pesoKg: Double,
+        val costoEnvio: Double = 15.0
+    ) : Producto(nombre, precio, cantidad) {
+
+        override fun calcularPrecioFinal(): Double {
+            return (precio * cantidad) + costoEnvio
+        }
+
+        override fun obtenerDetalle(): String {
+            return String.format("%-25s x%-2d  S/ %8.2f  [Fisico: Envio S/ %.2f]", nombre, cantidad, calcularPrecioFinal(), costoEnvio)
+        }
+    }
+
+    class ProductoDigital(
+        nombre: String,
+        precio: Double,
+        cantidad: Int,
+        val urlDescarga: String,
+        val comisionLicencia: Double = 0.0
+    ) : Producto(nombre, precio, cantidad) {
+
+        override fun calcularPrecioFinal(): Double {
+            return (precio + comisionLicencia) * cantidad
+        }
+
+        override fun obtenerDetalle(): String {
+            return String.format("%-25s x%-2d  S/ %8.2f  [Digital: Licencia S/ %.2f]", nombre, cantidad, calcularPrecioFinal(), comisionLicencia)
+        }
+    }
 }
